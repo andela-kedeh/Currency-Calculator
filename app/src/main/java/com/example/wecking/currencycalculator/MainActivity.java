a789b53960515ee20dd9dfcef4e6e814f608b046
andela.kedeh <kingsley.edeh@andela.co>
2015-09-26 19:59:42 +0100

    refactored and added the API integration for exchange rate

--- app/src/main/java/com/example/wecking/currencycalculator/MainActivity.java
+++ app/src/main/java/com/example/wecking/currencycalculator/MainActivity.java
@@ -4,34 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    private EditText showEntry;
+    private EditText showEntry, showEntry2;
     private EditText showResult;
-    private Button one;
-    private Button two;
-    private Button three;
-    private Button four;
-    private Button five;
-    private Button six;
-    private Button seven;
-    private Button eight;
-    private Button nine;
-    private Button zero;
-    private Button multi;
-    private Button plus;
-    private Button divide;
-    private Button minus;
-    private Button equal;
-    private Button clear;
-    private String str ="";
-    private int i,num,numtemp;
-    private Character operator = 'q';
-    private String entry = "";
+    private Double num;
+    private int i,num2,numtemp;
+    private static String entry = "", entry2 = "";
+    private Spinner currenciesResult, operator;
+    private String[] listCurrenciesResult, getListCurrenciesEntry, getListCurrenciesEntry2, listOperator;
+    private Spinner currenciesEntry;
+    private static String currencyEntry, currencyResult, operate, currencyEntry2;
+    private Button Ans, equal, clear;
 
+    private static final String API_URL = "https://openexchangerates.org/api/latest.json?app_id=bfc41d831a3040ffa50dd31bd0b72da2";//PUT YOUR API KEY HERE
+    private  Double rate;
+
+    private Spinner currenciesEntry2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,40 +43,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initialize() {
-        showEntry = (EditText) findViewById(R.id.entry);
+        currenciesEntry = (Spinner) findViewById(R.id.currencies_entry);
+        getListCurrenciesEntry = getResources().getStringArray(R.array.currencies_entry);
+        ArrayAdapter<String> entryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getListCurrenciesEntry);
+        currenciesEntry.setAdapter(entryAdapter);
+
+        currenciesEntry2 = (Spinner) findViewById(R.id.currencies_entry2);
+        getListCurrenciesEntry2 = getResources().getStringArray(R.array.currencies_entry2);
+        ArrayAdapter<String> entryAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, getListCurrenciesEntry2);
+        currenciesEntry2.setAdapter(entryAdapter2);
+
+        currenciesResult = (Spinner) findViewById(R.id.currencies_result);
+        listCurrenciesResult = getResources().getStringArray(R.array.currencies_result);
+        ArrayAdapter<String> resultAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listCurrenciesResult);
+        currenciesResult.setAdapter(resultAdapter);
+
+        operator = (Spinner) findViewById(R.id.operator);
+        listOperator = getResources().getStringArray(R.array.operator);
+        ArrayAdapter<String> operatorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, listOperator);
+        operator.setAdapter(operatorAdapter);
+
         showResult = (EditText) findViewById(R.id.result_id);
-        one = (Button) findViewById(R.id.Btn1_id);
-        one.setOnClickListener(this);
-        two = (Button) findViewById(R.id.Btn2_id);
-        two.setOnClickListener(this);
-        three = (Button) findViewById(R.id.Btn3_id);
-        three.setOnClickListener(this);
-        four = (Button) findViewById(R.id.Btn4_id);
-        four.setOnClickListener(this);
-        five = (Button) findViewById(R.id.Btn5_id);
-        five.setOnClickListener(this);
-        six = (Button) findViewById(R.id.Btn6_id);
-        six.setOnClickListener(this);
-        seven = (Button) findViewById(R.id.Btn7_id);
-        seven.setOnClickListener(this);
-        eight = (Button) findViewById(R.id.Btn8_id);
-        eight.setOnClickListener(this);
-        nine = (Button) findViewById(R.id.Btn9_id);
-        nine.setOnClickListener(this);
-        zero = (Button) findViewById(R.id.Btn0_id);
-        zero.setOnClickListener(this);
-        multi = (Button) findViewById(R.id.Btnmulti_id);
-        multi.setOnClickListener(this);
-        plus = (Button) findViewById(R.id.Btnplus_id);
-        plus.setOnClickListener(this);
-        divide = (Button) findViewById(R.id.Btndivide_id);
-        divide.setOnClickListener(this);
-        minus = (Button) findViewById(R.id.Btnminus_id);
-        minus.setOnClickListener(this);
-        equal = (Button) findViewById(R.id.buttonAns);
-        equal.setOnClickListener(this);
+        showEntry = (EditText) findViewById(R.id.entry);
+        showEntry2 = (EditText) findViewById(R.id.entry2);
+
+        Ans = (Button) findViewById(R.id.buttonAns);
+        Ans.setOnClickListener(this);
         clear = (Button) findViewById(R.id.Btnclear_id);
         clear.setOnClickListener(this);
+        equal = (Button) findViewById(R.id.equal);
+        equal.setOnClickListener(this);
     }
 
     @Override
@@ -102,118 +100,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.Btn1_id:
-                insert(1);
-                break;
-            case R.id.Btn2_id:
-                insert(2);
-                break;
-            case R.id.Btn3_id:
-                insert(3);
-                break;
-            case R.id.Btn4_id:
-                insert(4);
-                break;
-            case R.id.Btn5_id:
-                insert(5);
-                break;
-            case R.id.Btn6_id:
-                insert(6);
-                break;
-            case R.id.Btn7_id:
-                insert(7);
-                break;
-            case R.id.Btn8_id:
-                insert(8);
-                break;
-            case R.id.Btn9_id:
-                insert(9);
-                break;
-            case R.id.Btn0_id:
-                insert(0);
-                break;
-            case R.id.Btnmulti_id:
-                operator = '*';
-                perform();
-                break;
-            case R.id.Btnplus_id:
-                operator = '+';
-                perform();
-                break;
-            case R.id.Btnminus_id:
-                operator = '-';
-                perform();
-                break;
-            case R.id.Btndivide_id:
-                operator = '/';
-                perform();
-                break;
+
             case R.id.Btnclear_id:
                 reset();
                 break;
             case R.id.buttonAns:
-                calculate();
+                useAnswer();
+                break;
+            case R.id.equal:
+                currencyEntry2 = currenciesEntry2.getSelectedItem().toString();
+                currencyEntry = currenciesEntry.getSelectedItem().toString();
+                currencyResult = currenciesResult.getSelectedItem().toString();
+                entry = showEntry.getText().toString();
+                entry2 = showEntry2.getText().toString();
+                get(currencyEntry, currencyEntry2, currencyResult);
                 break;
         }
     }
-    private void insert(int j) {
-        str = str+Integer.toString(j);
-        num = Integer.valueOf(str).intValue();
-        showResult.setText(str);
-        showEntry.setText(entry);
+
+    private void useAnswer() {
+       String useValue = showResult.getText().toString();
+        showEntry.setText(useValue);
+        showResult.setText("");
+
+        currenciesResult.getSelectedItem();
     }
 
-    private void perform() {
-        numtemp = num;
-        if(operator == '+') {
-            entry = entry + str + '+';
-            showEntry.setText(entry);
+    public void calculate(Double firstRate, Double secondRate, Double resultRate) {
+        operate = operator.getSelectedItem().toString();
+        System.out.println(secondRate + "king2");
+        Double rate1 = resultRate / firstRate;
+        System.out.println(firstRate + "king1");
+        Double rate2 = resultRate / secondRate;
+        if(operate.equals("Add")) {
+            num = (rate1 * Double.valueOf(entry)) + (rate2 * Double.valueOf(entry2));
         }
-        else if(operator == '-') {
-            entry = entry + str + '-';
-            showEntry.setText(entry);
+        else if(operate.equals("Minus")) {
+            num = (rate1 * Double.valueOf(entry)) - (rate2 * Double.valueOf(entry2));
         }
-        else if(operator == '/') {
-            entry = entry + str + '/';
-            showEntry.setText(entry);
+        else if(operate.equals("Divide")) {
+            num = (rate1 * Double.valueOf(entry)) / (rate2 * Double.valueOf(entry2));
         }
-        else if(operator == '*') {
-            entry = entry + str + '*';
-            showEntry.setText(entry);
+        else if(operate.equals("Multiply")) {
+            num = (rate1 * Double.valueOf(entry)) * (rate2 * Double.valueOf(entry2));
         }
-        str = "";
-    }
-
-    private void calculate() {
-        if(operator == '+') {
-            num = numtemp + num;
-            showEntry.setText(entry);
-        }
-        else if(operator == '-') {
-            num = numtemp - num;
-            showEntry.setText(entry);
-        }
-        else if(operator == '/') {
-            num = numtemp / num;
-            showEntry.setText(entry);
-        }
-        else if(operator == '*') {
-            num = numtemp * num;
-            showEntry.setText(entry);
-        }
-        showResult.setText(""+num);
-        entry = entry + num;
-        showEntry.setText(entry);
-        num = 0;
-        numtemp = 0;
+        showResult.setText(String.valueOf(num));
     }
 
     private void reset() {
-        str ="";
-        operator ='q';
-        num = 0;
-        numtemp = 0;
+        num = 0.0;
         showResult.setText("");
         showEntry.setText("");
+        showEntry2.setText("");
+    }
+
+
+
+    public Double get(final String currency, final String currency2, final String currency3) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(API_URL, new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                try {
+                    String response = new String(responseBody, "UTF-8");
+                    JSONObject jsonObj = new JSONObject(response);
+                    JSONObject ratesObject = jsonObj
+                            .getJSONObject("rates");
+
+                    Double firstRate = ratesObject.getDouble(currency);
+                    Double secondRate = ratesObject.getDouble(currency2);
+                    Double thirdRate = ratesObject.getDouble(currency3);
+                    calculate(firstRate, secondRate, thirdRate);
+                    System.out.println(firstRate);
+
+                } catch (Exception e) {
+
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+
+        return rate;
     }
 }
