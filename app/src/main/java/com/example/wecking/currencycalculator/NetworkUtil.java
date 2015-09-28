496f2235760ddda28cddabe3ecafd08cee29fc3f
andela.kedeh <kingsley.edeh@andela.co>
2015-09-28 15:23:42 +0100

    added the ExchangeRate class and NetworkUtil class

---
+++ app/src/main/java/com/example/wecking/currencycalculator/NetworkUtil.java
@@ -0,0 +1,28 @@
+package com.example.wecking.currencycalculator;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.TextView;
+
+/**
+ * Created by andela on 8/7/15.
+ */
+public class NetworkUtil {
+
+    public static boolean getConnectivityStatus(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                return true;
+
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                return true;
+        }
+        return false;
+    }
+
+}
